db047552694d97a2f8956d72c757c8a4344f233d
Oliver <opaucarq@uni.pe>
2023-07-14 12:28:24 -0500

    update create page

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
+
+import Form from '../components/Form'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Create = () => {
+  const [formValues, setFormValues] = useState({});
+
+  const handleFormSubmit = (formData) => {
+    // console.log(formData);
+    setFormValues(formData);
+  };
+
   return (
-    <div>Create</div>
+    <>
+      <Header>Create Product</Header>
+      <Form onSubmit={handleFormSubmit}></Form>
+      <Footer formValues={formValues}>create</Footer>
+    </>
   )
 }
 
