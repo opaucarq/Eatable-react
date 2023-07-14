794d215c236c95b64034f9a099a5ae580504321e
Oliver <opaucarq@uni.pe>
2023-07-14 12:29:02 -0500

    update edit page

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import Form from '../components/Form'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Edit = () => {
+  const { id } = useParams();
+  const dataProduct = JSON.parse(localStorage.getItem("product"));
+  const [data, setData] = useState({dataProduct});
+  // console.log(data.dataProduct)
+  const [formValues, setFormValues] = useState({});
+
+  const handleFormSubmit = (formData) => {
+    // console.log(formData);
+    setFormValues(formData);
+  };
+
   return (
-    <div>Edit</div>
+    <>
+      <Header>Edit Product</Header>
+      <Form data = {data.dataProduct} onSubmit={handleFormSubmit}></Form>
+      <Footer formValues={formValues}>edit</Footer>
+    </>
   )
 }
 
