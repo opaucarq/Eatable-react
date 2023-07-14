5f22804365b3d3e497bb656a01bfbcfde9d655de
Oliver <opaucarq@uni.pe>
2023-07-14 11:48:33 -0500

    update: dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import styled from '@emotion/styled';
+import React, { useEffect, useState } from 'react';
+
+import FoodCard from './FoodCard';
+import index from '../services/products';
+
+const DashboardContainer = styled.div`
+width: 100%;
+`;
+const DashboardWrapper = styled.div`
+gap: 20px;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+`;
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default Dashboard
+  useEffect(() => {
+    
+    const getData = async () => {
+      const responseData = await index();
+      setData(responseData);
+    };
+
+    const interval = setInterval(getData, 200);
+
+    return () => {
+      clearInterval(interval);
+    };
+  },[]);
+
+  return (
+    <DashboardContainer>
+      <DashboardWrapper>
+        {data.length > 0 ? (
+          data.map((food) => <FoodCard key={food.id} food={food} />)
+        ) : (
+          <p style={{fontSize:"32px"}}>Loading...</p>
+        )}
+      </DashboardWrapper>
+    </DashboardContainer>
+  );
+};
+
+export default Dashboard;
