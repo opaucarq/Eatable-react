780c308542df3d7080b9f16ec8695c8627e9eb7d
Oliver <opaucarq@uni.pe>
2023-07-14 12:32:27 -0500

    add main web page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Global } from '@emotion/react';
+
 import App from './App'
-import './index.css'
+import { global, reset } from "./styles"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Global styles = { reset } />
+    <Global styles = { global } />
     <App />
   </React.StrictMode>,
 )
