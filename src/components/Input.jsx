69aaa10873bc6668382688a75be7eec0d4e04b05
Oliver <opaucarq@uni.pe>
2023-07-14 12:20:10 -0500

    update input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
 
-const Input = () => {
+import { typography } from '../styles'
+
+const Field = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background: transparent;
+  label {
+    color: #B8B8BB;
+    margin: 16px 0 8px;
+    ${typography.text.xs}
+  }
+
+  input {
+    border: none;
+    outline: none;  
+    background: transparent; 
+    border-bottom: 1px solid black;
+  }
+`;
+
+const Input = ({ label, name, value, onChange }) => {
   return (
-    <div>Input</div>
-  )
-}
+    <Field>
+      <label htmlFor={name}>{label}</label>
+      <input style={{"background":"transparent"}}
+        type="text"
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </Field>
+  );
+};
 
-export default Input
+export default Input;
