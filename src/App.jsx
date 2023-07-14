03f65f2dc68eccee7cbd6ac27b240d6b4b5f9980
Oliver <opaucarq@uni.pe>
2023-07-14 12:33:42 -0500

    update add.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Edit from "./pages/Edit";
+import Main from "./pages/Main";
+import Create from "./pages/Create";
+import Product from "./pages/Product";
 
 function App() {
+  
   return (
-    <>
-    </>
-  )
+    <div style={{ minHeight: "calc(100% - 200px)", paddingBottom: "100px" }}>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path="/" 
+            element = {
+              <>
+                <Main></Main>
+              </>
+            }
+          />
+          <Route 
+            path="/show/:id" 
+            element={
+              <>
+                <Product></Product>
+              </>
+            }
+          />
+          <Route 
+            path="/edit/:id" 
+            element = {
+              <Edit></Edit>
+            } 
+          />
+          <Route
+            path="/create"
+            element = {
+              <Create></Create>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
